224eb0df7a49d1b06a42db59910c86d024917514
Māris Zinbergs <maris.zinbergs@gmail.com>
2021-03-05 21:53:40 +0200

    Uzlabots P1 -> Specifikācija -> Ievārījums piemērs, pievienots JS variants

--- src/vidusskola/p1/python/ievarijums1.py
+++ src/vidusskola/p1/python/ievarijums1.py
@@ -1,4 +1,8 @@
 def ievarijums(aboli_svars, cukurs_uz_kg):
-    izmaksa_kg = 0.75 * cukurs_uz_kg
-    izmaksas = izmaksa_kg * aboli_svars
-    print(izmaksas)
+    cukura_cena = 0.75
+    izmaksa_kg = cukura_cena * cukurs_uz_kg
+    return izmaksa_kg * aboli_svars
+
+aboli = 1.5
+cukurs = 0.7
+print("Uz {} kg ābolu izmaksas būs {} EUR".format(aboli, ievarijums(aboli, cukurs)))
--- src/vidusskola/p1/python/ievarijums2.py
+++ src/vidusskola/p1/python/ievarijums2.py
@@ -5,9 +5,13 @@ def izmaksas_receptei(recepte, cena):
     return summa
 
 def izmaksas_kopa(abolu_svars):
-    recepte1 = {“cukurs”: 0.6, “kanelis”: 0.08, “aboli”: 2.0, “udens”: 0.2}
-    cenas = {“cukurs”: 0.75, “kanelis”: 30.0, “aboli”: 0.0, “udens”: 0.0}
-    izmaksas_kg = izmaksas_receptei(recepte1, cenas) / recepte[“aboli”]
-    ievarijuma_izmaksas = svars * izmaksas_kg
+    recepte = {"cukurs": 0.6, "kanelis": 0.08, "aboli": 2.0, "udens": 0.2}
+    cenas = {"cukurs": 0.75, "kanelis": 30.0, "aboli": 0.0, "udens": 0.0}
+    izmaksas_kg = izmaksas_receptei(recepte, cenas) / recepte["aboli"]
+    return abolu_svars * izmaksas_kg
 
-    print(“Uz {} kg ābolu izmaksas būs {}”.format(abolu_svars, ievarijuma_izmaksas))
+aboli = 1.5
+print("Uz {} kg ābolu izmaksas būs {} EUR".format(aboli, izmaksas_kopa(aboli)))
+
+aboli = 5
+print("Uz {} kg ābolu izmaksas būs {} EUR".format(aboli, izmaksas_kopa(aboli)))
